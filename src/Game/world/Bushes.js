7165af6c6581f02835ea6bad05b5b499ea92f7ae
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2026-04-01 19:03:53 +0100

    bushes position in a new file

--- src/Game/world/Bushes.js
+++ src/Game/world/Bushes.js
@@ -1,6 +1,7 @@
 import * as THREE from "three"
 import Game from "../Game"
 import Foliage from "./Foliage"
+import { bushes } from "./mapConfig"
 
 export default class Bush {
   constructor() {
@@ -19,13 +20,7 @@ export default class Bush {
       noiseTexture: this.resources.items.perlinTexture,
     })
 
-    this.positions = [
-      { x: 3, z: 2, scale: 0.7 },
-      { x: -5, z: 8, scale: 0.5 },
-      { x: 10, z: -3, scale: 0.8 },
-      { x: -2, z: -6, scale: 0.6 },
-      { x: 7, z: 5, scale: 0.9 },
-    ]
+    this.positions = bushes
 
     this.setMesh()
     this.setDebug()
---
+++ src/Game/world/mapConfig.js
@@ -0,0 +1,7 @@
+export const bushes = [
+  { x: 3, z: 2, scale: 0.7 },
+  { x: -5, z: 8, scale: 0.5 },
+  { x: 10, z: -3, scale: 0.8 },
+  { x: -2, z: -6, scale: 0.6 },
+  { x: 7, z: 5, scale: 0.9 },
+]
